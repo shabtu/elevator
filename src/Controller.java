be5927f6f7002a1a6463a13fc9ea41f4b84df99e
tobiaj <tobiaj@kth.se>
2016-03-18 13:48:30 +0100

    Merge branch 'master' of https://github.com/shabtu/elevator
    # Conflicts:
    #	src/Controller.java
    #	src/Worker.java

--- src/Controller.java
+++ src/Controller.java
@@ -10,57 +10,106 @@ import java.net.Socket;
 public class Controller {
     public static Socket socket;
     public static PrintWriter output;
-            public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
 
-                BufferedReader input;
-                String[] arguments;
+        BufferedReader input;
+        String[] arguments;
+        boolean done = false;
+        int fallSafeCounter = 0;
 
-
-                socket = new Socket("localhost", 4711);
-                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                output = new PrintWriter(socket.getOutputStream(), true);
+        socket = new Socket("localhost", 4711);
+        input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        output = new PrintWriter(socket.getOutputStream(), true);
 
 
                 /*Create workers to handle elevators*/
-                Worker[] workers = new Worker[3];
+        Worker[] workers = new Worker[3];
 
 
-                for (int i=0; i < workers.length ; i++){
-                    workers[i] = new Worker(i+1);
-                    workers[i].start();
-                }
+        for (int i=0; i < workers.length ; i++){
+            workers[i] = new Worker(i+1);
+            workers[i].start();
+        }
 
-                String inputArgument;
+        String inputArgument;
 
 		/*Reading input from elevator buttons*/
-                while ((inputArgument = input.readLine()) != null) {
-                    if ("".equals(input)) {
-                        continue;
-                    }
-                    arguments = inputArgument.split(" ");
+        while ((inputArgument = input.readLine()) != null) {
+            if ("".equals(input)) {
+                continue;
+            }
+            arguments = inputArgument.split(" ");
 
-                    if (arguments[0].equals("v"))
-                        continue;
-                    else if (arguments[0].equals("b")){
-                        for (int i=0; i < workers.length; i++){
-                            if (!workers[i].busy){
-                                workers[i].setInput(inputArgument);
-                                break;
+            if (arguments[0].equals("v"))
+                continue;
+            else if (arguments[0].equals("b")){
+                for (int i=0; i < workers.length; i++){
+                    if (!workers[i].busy){
+                        System.out.println(inputArgument);
+                        workers[i].setInput(inputArgument);
+                        done = true;
+                        break;
+                    }
+                }
+                if (!done){
+                    for (int i=0; i < workers.length; i++ ){
+                        if(workers[i].up == (Integer.parseInt(arguments[2]) == 1)){
+                            if(workers[i].up){
+                                if (workers[i].position < Double.parseDouble(arguments[1])){
+                                    workers[i].setInput(inputArgument);
+                                    done = true;
+                                    break;
+                                }
+                            }
+                            else {
+                                if (workers[i].position > Double.parseDouble(arguments[1])){
+                                    workers[i].setInput(inputArgument);
+                                    done = true;
+                                    break;
+                                }
                             }
                         }
                     }
-                    else if (inputArgument.equals("p " + Integer.parseInt(arguments[1]) + " 32000")){
-                        Controller.output.println("m " + Integer.parseInt(arguments[1]) + " 0");
-                        System.out.println("Stopped worker " + Integer.parseInt(arguments[1]));
-                        workers[Integer.parseInt(arguments[1])-1].busy = false;
-                        continue;
-                    }
-                    else if (arguments[0].equals("f")){
-                        workers[Integer.parseInt(arguments[1])-1].setInput(inputArgument);
-                    }
-                    else if (!workers[Integer.parseInt(arguments[1])-1].busy)
-                        workers[Integer.parseInt(arguments[1])-1].setInput(inputArgument);
                 }
+                if (!done) {
+                    int counter = 0;
+                    String x = "b " + fallSafeCounter + " " + arguments[1];
+                    if (workers[fallSafeCounter].up) {
+                        for (String n : workers[fallSafeCounter].listDown) {
+                            if (Double.parseDouble(n.split(" ")[2]) > Double.parseDouble(arguments[1])) {
+                                continue;
+                            }
+                            counter++;
+                        }
+                        workers[fallSafeCounter].listDown.add(counter, x);
+                    }
+                    else {
+                        for (String n : workers[fallSafeCounter].listUp) {
+                            if (Double.parseDouble(n.split(" ")[2]) > Double.parseDouble(arguments[1])) {
+                                continue;
+                            }
+                            counter++;
+                        }
+                        workers[fallSafeCounter].listUp.add(counter, x);
+                    }
+
+                    fallSafeCounter++;
+                    fallSafeCounter = fallSafeCounter % 3;
+                }
+                done = false;
             }
+            else if (inputArgument.equals("p " + Integer.parseInt(arguments[1]) + " 32000")){
+                workers[Integer.parseInt(arguments[1])-1].setInput(inputArgument);
+                Controller.output.println("m " + Integer.parseInt(arguments[1]) + " 0");
+                System.out.println("Stopped worker " + Integer.parseInt(arguments[1]));
+                workers[Integer.parseInt(arguments[1])-1].busy = false;
+                continue;
+            }
+            else if (arguments[0].equals("f")){
+                workers[Integer.parseInt(arguments[1])-1].setInput(inputArgument);
+            }
+            else workers[Integer.parseInt(arguments[1])-1].setInput(inputArgument);
         }
+    }
+}
--- src/Worker.java
+++ src/Worker.java
@@ -4,6 +4,8 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.DoubleSummaryStatistics;
+import java.util.LinkedList;
+import java.util.concurrent.Semaphore;
 
 /**
  * Created by Simon on 2016-03-14.
@@ -11,13 +13,17 @@ import java.util.DoubleSummaryStatistics;
 public class Worker extends Thread{
 
     int elevatorID;
-    double position = 0, destination = 0, floor = 0;
+    double position = 0, destination = -1, next, lastDestination = -1;
     String[] arguments;
+    String[] temp;
+    String[] tempF;
     String input = "";
-    boolean busy = false;
+    boolean busy = false,stop = false, up = true;
+    //Semaphore lock = new Semaphore(1, true);
 
-    Socket socket;
-    PrintWriter output;
+    LinkedList <String> listUp = new LinkedList<>();
+    LinkedList <String> listDown = new LinkedList<>();
+
 
     public Worker(int elevatorID) throws IOException {
         this.elevatorID = elevatorID;
@@ -31,18 +37,190 @@ public class Worker extends Thread{
         }
     }
 
-    public synchronized void setInput(String input){
+    public synchronized void setInput(String input) {
         this.input = input;
-        arguments = input.split(" ");
+        temp = input.split(" ");
+        int counter = 0;
+
+        if(temp[2].equals("32000")) {
+            stop = true;
+            listUp.clear();
+            listDown.clear();
+            busy = false;
+        }
+        else if (temp[0].equals("b")) {
+            double wantedDestination = Double.parseDouble(temp[1]);
+
+            if (!busy) {
+                String a = "p " +  + elevatorID + " " + temp[1];
+                if ((wantedDestination - position) > 0) {
+                    listUp.addFirst(a);
+                    up = true;
+                }
+                else {
+                    listDown.addLast(a);
+                    up = false;
+                }
+            }
+            else if (up) {
+                if(destination < wantedDestination) {
+                    counter = 0;
+                    for(String n : listUp) {
+                        if (Double.parseDouble(n.split(" ")[2]) > wantedDestination) {
+                            System.out.println("firsta " + next + " andra " + wantedDestination);
+                            continue;
+                        }
+                        counter++;
+                    }
+                    String a = "p " + elevatorID + " " + temp[1];
+                    listUp.add(counter, a);
+                }
+                else {
+                    String a = "p " +  + elevatorID + " " + destination;
+                    listUp.addFirst(a);
+                    destination = wantedDestination;
+                }
+            }
+            else {
+                if(destination > wantedDestination) {
+                    counter = 0;
+                    for(String n : listDown) {
+                        if (Double.parseDouble(n.split(" ")[2]) > wantedDestination) {
+                            System.out.println("firsta " + next + " andra " + wantedDestination);
+                            continue;
+                        }
+                        counter++;
+                    }
+                    String a = "p " + elevatorID + " " + temp[1];
+                    listDown.add(counter, a);
+                }
+                else {
+                    String a = "p " +  + elevatorID + " " + destination;
+                    listUp.addLast(a);
+                    destination = wantedDestination;
+                }
+            }
+        }
+        else if(!temp[0].equals("f")) {
+            double wantedDestination = Double.parseDouble(temp[2]);
+            if (destination == wantedDestination);
+            else if(destination < wantedDestination) {
+
+                if (up){
+                    counter = 0;
+                    for(String n : listUp) {
+                        if (Double.parseDouble(n.split(" ")[2]) > wantedDestination) {
+                            System.out.println("firsta " + next + " andra " + wantedDestination);
+                            continue;
+                        }
+                        counter++;
+                    }
+                    listUp.add(counter, input);
+                }
+                else if (!up){
+                    counter = 0;
+                    if (position < wantedDestination){
+                        for(String n : listUp) {
+                            if (Double.parseDouble(n.split(" ")[2]) > wantedDestination) {
+                                System.out.println("firsta " + next + " andra " + wantedDestination);
+                                continue;
+                            }
+                            counter++;
+                        }
+
+                        listUp.add(counter, input);
+
+                    }
+                    else {
+                        String p = ("p " + elevatorID + " " + destination);
+                        listDown.addLast(p);
+                        destination = wantedDestination;
+                    }
+                }
+            }
+            else if (destination > wantedDestination){
+                if (up){
+                    String p = ("p " + elevatorID + " " + destination);
+                    if (position < wantedDestination){
+                        listUp.addFirst(p);
+
+                        destination = wantedDestination;
+                    }
+                    else {
+                        counter = 0;
+                        for(String n : listDown) {
+                            if (Double.parseDouble(n.split(" ")[2]) > wantedDestination) {
+                                continue;
+                            }
+                            counter++;
+                        }
+
+                        listDown.add(counter, input);
+                    }
+
+                }
+                else if (!up){
+                    counter = 0;
+                    for(String n : listDown) {
+                        if (Double.parseDouble(n.split(" ")[2]) > wantedDestination) {
+                            System.out.println("firsta " + next + " andra " + wantedDestination);
+                            continue;
+                        }
+                        counter++;
+                    }
+
+                    listDown.add(counter, input);
+                }
+            }
+        }
+
+        else tempF = input.split(" ");
+
         notify();
     }
 
     public synchronized void handleInput() throws InterruptedException {
 
         while (true) {
-            wait();
-            System.out.println("Worker " + elevatorID + " got message: " + input);
+
+            System.out.println();
+
+            if(!busy || stop) {
+                stop = false;
+                if(listUp.isEmpty() && listDown.isEmpty()) {
+                    wait();
+                    while (temp[0].equals("f")) wait();
+                }
+
+                busy = true;
+                if (up) {
+                    if (!listUp.isEmpty()) {
+                        arguments = listUp.removeFirst().split(" ");
+                    }
+                    else if (!listDown.isEmpty()) {
+                        up = false;
+                        arguments = listDown.removeLast().split(" ");
+                    }
+                }
+                else if (!up){
+                    if (!listDown.isEmpty()) {
+                        arguments = listDown.removeLast().split(" ");
+                    }
+                    else if (!listUp.isEmpty()){
+                        up = true;
+                        arguments = listUp.removeFirst().split(" ");
+                    }
+                }
+
+            }
+            else {
+                wait();
+                arguments = tempF;
+            }
             busy = true;
+
+            System.out.println("Worker " + elevatorID + " got message: " + arguments[0] + " " + arguments[1] + " " + arguments[2]);
+
 
             if (arguments[0].equals("p")){
 
@@ -72,20 +250,21 @@ public class Worker extends Thread{
                 if ((destination-0.05) < position && position < (destination+0.05)) {
                     Controller.output.println("m " + elevatorID + " 0");
                     Controller.output.println("d " + elevatorID + " 1");
+                    sleep(1000);
                     busy = false;
                 }
             }
             if (arguments[0].equals("b")){
                 Controller.output.println("d " + elevatorID + " -1");
 
-                destination = Double.parseDouble(arguments[1]);
+                destination = Double.parseDouble(arguments[2]);
 
-                System.out.println("Någon vill åka från: " + destination);
-
+                System.out.println("Nï¿½gon vill ï¿½ka frï¿½n: " + destination);
                 if (destination < position)
                     Controller.output.println("m " + elevatorID + " -1");
 
                 if (destination == position){
+                    Controller.output.println("d " + elevatorID + " 1");
                     busy = false;
                     continue;
                 }
@@ -94,6 +273,8 @@ public class Worker extends Thread{
                     Controller.output.println("m " + elevatorID + " 1");
 
             }
+
+
         }
     }
 }
